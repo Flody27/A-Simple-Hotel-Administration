a1d48c4690b38ec31a8811047dfc1ecd0ad527af
Enzo1504 <emolinari00383@ufide.ac.cr>
2023-03-06 20:31:44 -0600

    Backend y DAL de Membresia y Controller

---
+++ ProyectoPrograAvanzadaWeb/BackEnd/Controllers/MembresiaController.cs
@@ -0,0 +1,76 @@
+﻿using DAL.Implementations;
+using DAL.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembresiaController : Controller
+    {
+        private IMembresiaDAL membresiaDAL;
+
+        #region Constructor
+        public MembresiaController ()
+        {
+            membresiaDAL = new MembresiaDALImpl(new Entities.Entities.HotelContext());
+        }
+        #endregion
+
+        #region Consultar
+        // GET: api/<CategoryController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            IEnumerable<Membresia> membresias = membresiaDAL.GetAll();
+
+            return new JsonResult(membresias);
+        }
+
+        // GET api/<CategoryController>/5
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            Membresia membresia;
+            membresia = membresiaDAL.Get(id);
+
+            return new JsonResult(membresia);
+        }
+        #endregion
+
+        #region Agregar
+        // POST api/<CategoryController>
+        [HttpPost]
+        public JsonResult Post([FromBody] Membresia membresia)
+        {
+            membresiaDAL.Add(membresia);
+            return new JsonResult(membresia);
+        }
+        #endregion
+
+        #region MOdificar
+        // PUT api/<CategoryController>/5
+        [HttpPut]
+        public JsonResult Put([FromBody] Membresia membresia)
+        {
+            membresiaDAL.Update(membresia);
+            return new JsonResult(membresia);
+        }
+        #endregion
+
+        #region Eliminar
+        // DELETE api/<CategoryController>/5
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            Membresia membresia = new Membresia{ MbrId = id };
+            membresiaDAL.Remove(membresia);
+
+            return new JsonResult(membresia);
+        }
+
+        #endregion
+    }
+}
+
---
+++ ProyectoPrograAvanzadaWeb/DAL/Implementations/MembresiaDALImpl.cs
@@ -0,0 +1,142 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class MembresiaDALImpl : IMembresiaDAL
+    {
+        HotelContext context;
+
+
+        public MembresiaDALImpl()
+        {
+            context = new HotelContext();
+
+        }
+
+        public MembresiaDALImpl(HotelContext _Context)
+        {
+            context = _Context;
+
+        }
+
+        public bool Add(Membresia entity)
+        {
+            try
+            {
+                using (UnidadDeTrabajo<Membresia> unidad = new UnidadDeTrabajo<Membresia>(context))
+                {
+                    unidad.genericDAL.Add(entity);
+                    unidad.Complete();
+                }
+
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public void AddRange(IEnumerable<Membresia> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Membresia> Find(Expression<Func<Membresia, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Membresia Get(int id)
+        {
+            Membresia membresias;
+            using (UnidadDeTrabajo<Membresia> unidad = new UnidadDeTrabajo<Membresia>(context))
+            {
+
+                membresias = unidad.genericDAL.Get(id);
+            }
+            return membresias;
+
+        }
+
+        public IEnumerable<Membresia> GetAll()
+        {
+            try
+            {
+                IEnumerable<Membresia> membresias;
+                using (UnidadDeTrabajo<Membresia> unidad = new UnidadDeTrabajo<Membresia>(context))
+                {
+                    membresias = unidad.genericDAL.GetAll();
+                }
+                return membresias;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Remove(Membresia entity)
+        {
+            bool result = false;
+            try
+            {
+                using (UnidadDeTrabajo<Membresia> unidad = new UnidadDeTrabajo<Membresia>(context))
+                {
+                    unidad.genericDAL.Remove(entity);
+                    result = unidad.Complete();
+                }
+
+            }
+            catch (Exception)
+            {
+
+                result = false;
+            }
+
+            return result;
+        }
+
+        public void RemoveRange(IEnumerable<Membresia> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Membresia SingleOrDefault(Expression<Func<Membresia, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Membresia entity)
+        {
+            bool result = false;
+
+            try
+            {
+                using (UnidadDeTrabajo<Membresia> unidad = new UnidadDeTrabajo<Membresia>(context))
+                {
+                    unidad.genericDAL.Update(entity);
+                    result = unidad.Complete();
+                }
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+            return result;
+        }
+    }
+}
