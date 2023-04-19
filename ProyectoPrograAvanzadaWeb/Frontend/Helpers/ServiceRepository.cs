d608c7f1596ea8910bf89e48b7e1ca6a6df5c9ad
Andres Chavarria <adrchb24@gmail.com>
2023-04-18 22:53:42 -0600

    Agregar APIKey al frontend

--- ProyectoPrograAvanzadaWeb/Frontend/Helpers/ServiceRepository.cs
+++ ProyectoPrograAvanzadaWeb/Frontend/Helpers/ServiceRepository.cs
@@ -8,6 +8,16 @@
         public ServiceRepository() { 
             Client = new HttpClient();
             Client.BaseAddress = new Uri("http://localhost:5013");
+            Client.DefaultRequestHeaders.Add("ApiKey", "12345");
+        }
+
+        public ServiceRepository(string token)
+        {
+            Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:5013");
+            Client.DefaultRequestHeaders.Add("ApiKey", "12345");
+            Client.DefaultRequestHeaders.Authorization =
+               new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
         }
 
         public HttpResponseMessage GetResponse(string url)
