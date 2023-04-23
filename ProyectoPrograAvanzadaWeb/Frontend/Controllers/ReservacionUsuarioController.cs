da2d47965bea75a514154a89fd54c9707b0fe4e0
Diego Andres Valle Valle <diegovv1300@gmail.com>
2023-04-23 15:22:50 -0600

    Correcion de reservaciones

--- ProyectoPrograAvanzadaWeb/Frontend/Controllers/ReservacionUsuarioController.cs
+++ ProyectoPrograAvanzadaWeb/Frontend/Controllers/ReservacionUsuarioController.cs
@@ -9,7 +9,8 @@ namespace Frontend.Controllers
     {
         private ReservacionHelper helper;
 
-        public ReservacionUsuarioController() { 
+        public ReservacionUsuarioController()
+        {
             helper = new ReservacionHelper();
         }
 
@@ -24,19 +25,6 @@ namespace Frontend.Controllers
             return View();
         }
 
-        // GET: ReservacionUsuarioController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        //// GET: ReservacionUsuarioController/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        // POST: ReservacionUsuarioController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(ReservacionViewModel model)
@@ -45,7 +33,7 @@ namespace Frontend.Controllers
             {
                 model.RsvUsrId = 1;
                 helper.Create(model);
-                return RedirectToAction("Index","Home");
+                return RedirectToAction("Index", "Home");
             }
             catch
             {
@@ -53,46 +41,10 @@ namespace Frontend.Controllers
             }
         }
 
-        // GET: ReservacionUsuarioController/Edit/5
-        public ActionResult Edit(int id)
-        {
+
+        public ActionResult ErrorReservacion(){
             return View();
-        }
+        } 
 
-        // POST: ReservacionUsuarioController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ReservacionUsuarioController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ReservacionUsuarioController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
